a4545bb24d3397c74afed1a360fc9a6884af9ce9
Kauã Sato Sposato <kaua.sato@gmail.com>
2025-06-06 15:12:46 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>Number analysis</header>
+      <section>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam saepe
+          optio, nisi ratione incidunt minima aspernatur consectetur suscipit
+          magni amet iure laboriosam nemo blanditiis? Voluptatum quam dolorum
+          dignissimos labore reiciendis.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
-  )
+  );
 }
-
-export default App
