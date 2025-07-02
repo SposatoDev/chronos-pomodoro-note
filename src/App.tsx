b9ebbcd984b1556b49ae5af95cb24dc7e5a4d422
Kauã Sato Sposato <kaua.sato@gmail.com>
2025-07-02 16:12:48 -0300

    Add theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Container } from "./components/Container/Container";
 import { Logo } from "./components/Logo/Logo";
 import { Menu } from "./components/Menu/Menu";
-
-import "./styles/theme.css";
-import "./styles/global.css";
 import { CountDown } from "./components/CountDown/Count";
 import { Forms } from "./components/Forms/Forms";
 import { Footer } from "./components/Footer/Footer";
+
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
   return (
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,20 +1,55 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import styles from "./Styles.module.css";
 
 export function Menu() {
+  type AvailableThemes = "dark" | "light";
+
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+  // useState utilizado para criar e controlar um estado dentro de um componente
+  // [Armazena o valor atual, function para atualizar esse valor]
+
+  function handleClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme == "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("Data-theme", theme);
+  }, [theme]); // Executa o useEffect apenas quando o theme muda.
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="History"
+        title="History"
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Settings"
+        title="Settings"
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Theme"
+        title="Theme"
+        onClick={handleClick}
+      >
         <SunIcon />
       </a>
     </nav>
