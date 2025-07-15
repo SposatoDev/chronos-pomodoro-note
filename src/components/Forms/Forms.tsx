9772bb41e4e80f202cf289cc1ac013c33749363d
Kauã Sato Sposato <kaua.sato@gmail.com>
2025-07-15 18:25:04 -0300

    Update on change in form

--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -5,29 +5,41 @@ import { DefaultInput } from "../DefaultInput/DefaultInput";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 import styles from "./Styles.module.css";
+import { useState } from "react";
 
 export function Forms() {
   const { state } = useTaskContext();
 
+  const [taskName, setTaskName] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
     <>
-      <form action="" className="form">
+      <form onSubmit={handleSubmit} action="" className="form">
         <div className={styles.formRow}>
-          <DefaultInput type="text" labeltext="Task" />
+          <DefaultInput
+            type="text"
+            labeltext="Task"
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.formRow}>
+          <p>Lorem ipsum dolor sit {state.config.worktime} amet.</p>
+        </div>
+
+        <div className={styles.formRow}>
+          <Cycle />
+        </div>
+
+        <div className={styles.formRow}>
+          <DefaultButton icon={<PlayCircleIcon />} />
         </div>
       </form>
-
-      <div className={styles.formRow}>
-        <p>Lorem ipsum dolor sit {state.config.worktime} amet.</p>
-      </div>
-
-      <div className={styles.formRow}>
-        <Cycle />
-      </div>
-
-      <div className={styles.formRow}>
-        <DefaultButton icon={<PlayCircleIcon />} />
-      </div>
     </>
   );
 }
