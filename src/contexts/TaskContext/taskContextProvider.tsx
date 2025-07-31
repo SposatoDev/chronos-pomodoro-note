4d735bb21e7a9e43bd9de871f439092cfa4d2f07
Kauã Sato Sposato <kaua.sato@gmail.com>
2025-07-31 00:04:13 -0300

    load and play beep when complete task

--- src/contexts/TaskContext/taskContextProvider.tsx
+++ src/contexts/TaskContext/taskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { initialState } from "./initialState";
 import { TaskContext } from "./taskContext";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 import { TaskActionType } from "./actionType";
+import { loadBeep } from "../../utils/loadBeep";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,12 +13,18 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialState);
 
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
+
   const worker = TimerWorkerManager.getInstance();
 
   worker.onmessage((e) => {
     const countDownSeconds = e.data;
 
     if (countDownSeconds === 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
       dispatch({ type: TaskActionType.COMPLETE_TASK });
       worker.terminate();
     } else {
@@ -30,13 +37,20 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   useEffect(() => {
     if (!state.activeTask) {
-      console.log(state);
-      console.log("Worker terminado por falta de activeTask");
+      console.log("Nenhuma task ativa, encerrando worker.");
       worker.terminate();
     }
 
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
