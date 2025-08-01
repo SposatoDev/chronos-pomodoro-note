eeab1ba09c4c043f81e17feac4ca8948d3220ab9
Kauã Sato Sposato <kaua.sato@gmail.com>
2025-07-31 23:08:34 -0300

    Add a router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import { Home } from "./pages/Home";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
 import { TaskContextProvider } from "./contexts/TaskContext/taskContextProvider";
 import { MessagesContainer } from "./components/Container/MessagesContainer";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { MainRouter } from "./routers/MainRouter";
 
-import "react-toastify/dist/ReactToastify.css";
 import "./styles/theme.css";
 import "./styles/global.css";
-import { NotFound } from "./pages/NotFound";
 
 export function App() {
   return (
     <>
       <TaskContextProvider>
         <MessagesContainer>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
+          <MainRouter />
         </MessagesContainer>
       </TaskContextProvider>
     </>
