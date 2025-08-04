b0d5fb15d0a6c50210156acc4b756f523da663bd
Kauã Sato Sposato <kaua.sato@gmail.com>
2025-08-04 15:05:20 -0300

    Recuperar o estado do localStorage

--- src/contexts/TaskContext/taskContextProvider.tsx
+++ src/contexts/TaskContext/taskContextProvider.tsx
@@ -5,13 +5,28 @@ import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 import { TaskActionType } from "./actionType";
 import { loadBeep } from "../../utils/loadBeep";
+import type { TaskStatemodel } from "../../models/TaskStateModel";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, dispatch] = useReducer(taskReducer, initialState);
+  const [state, dispatch] = useReducer(taskReducer, initialState, () => {
+    // Será utilizado para armazenar as tasks que foram abandonadas para notificar no histórico.
+    const storageState = localStorage.getItem("state");
+
+    if (storageState == null) return initialState;
+
+    const parsedStorageState = JSON.parse(storageState) as TaskStatemodel; // Tenta converter o estado armazenado no navegador de string para o tipo modelo do estado.
+
+    return {
+      ...parsedStorageState,
+      activeTask: null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: "00:00",
+    };
+  });
 
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
@@ -36,6 +51,8 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   });
 
   useEffect(() => {
+    localStorage.setItem("state", JSON.stringify(state));
+
     if (!state.activeTask) {
       console.log("Nenhuma task ativa, encerrando worker.");
       worker.terminate();
